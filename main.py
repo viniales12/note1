4e3f2420bc365a4671bfae7f891fdb654ce101d0
Viniales12 <viniales12@gmail.com>
2022-08-07 17:03:45 +0200

    .

--- main.py
+++ main.py
@@ -1,26 +1,66 @@
 import sqlite3
 
+
 def connect_to_db():
     conn = sqlite3.connect('database.db')
     return conn
+
 
 def create_db_table():
     try:
         conn = connect_to_db()
         conn.execute(''') 
             CREATE TABLE users (
-            user_id INTEGER PRIMARY KEY NOT NULL,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            phone TEXT NOT NULL,
-            address TEXT NOT NULL,
-            country TEXT NOT NULL,
+                user_id INTEGER PRIMARY KEY NOT NULL,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                address TEXT NOT NULL,
+                country TEXT NOT NULL,
             );
         ''')
         conn.commit()
-        print("User  table created successfully")
+        print("User table created successfully")
     except:
         print("User table creation failed - Maybe table")
     finally:
         conn.close()
+
+
+def insert_user(user):
+    inserted_user = {}
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute('''
+        INSERT INTO users (name, email, phone, address, country) VALUES (?, ?, ?, ?, ?)
+        ''', (user['name'], user['email'], user['phone'], user['address'], user['country']))
+        conn.commit()
+        inserted_user = get_user_by_id(cur.lastrowid)
+    except:
+        conn().rollback()
+    finally:
+        conn.close()
+
+    return inserted_user
+
+
+user = {
+    "name": "John Doe",
+    "email": "jondoe@gmail.com",
+    "phone": "06444944",
+    "address": "John Doe Street, Innsbruck",
+    "country": "Austria"
+}
+
+
+def get_users():
+    users = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        # ^ zamienia tuple na słownik?
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM users")
+        rows = cur.fetchall()
 
