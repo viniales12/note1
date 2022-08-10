022661ad6e22d94d163a0231efa19d2d80c77585
Viniales12 <viniales12@gmail.com>
2022-08-10 17:18:58 +0200

    + error handling

--- db/db_management.py
+++ db/db_management.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def connect_db():
-    conn = sqlite3.connect('my_db.db')
+    conn = sqlite3.connect('db/my_db.db')
     return conn
 
 
@@ -23,17 +23,29 @@ def create_db():
 def add_user(user):
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute('''
-    INSERT INTO users (name, email, phone, address,       
-                    country) VALUES (?, ?, ?, ?, ?)
-    ''', (user['name'],
-          user['email'], user['phone'], user['address'],
-          user['country']))
+    try:
+        cur.execute('''
+        INSERT INTO users (name, email, phone, address,       
+                       country) VALUES (?, ?, ?, ?, ?)
+        ''', (user['name'],
+              user['email'], user['phone'], user['address'],
+              user['country']))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+    except:
+        create_db()
+        cur.execute('''
+                INSERT INTO users (name, email, phone, address,       
+                               country) VALUES (?, ?, ?, ?, ?)
+                ''', (user['name'],
+                      user['email'], user['phone'], user['address'],
+                      user['country']))
 
-    return f"New User {user['name']} Successfully added"
+        conn.commit()
+        conn.close()
+    finally:
+        return f"New User {user['name']} Successfully added"
 
 
 def show_db():
@@ -44,17 +56,29 @@ def show_db():
     #  ^^^^^  umożliwia dostęp do kolumn za pomocą nazw, podczas gdy zwykła krotka wymagałaby użycia indeksów numerowanych
     cur = conn.cursor()
 
-    for i in cur.execute('''SELECT * FROM users'''):
-        user = {}
-        user["user_id"] = i["user_id"]
-        user["name"] = i["name"]
-        user["email"] = i["email"]
-        user["phone"] = i["phone"]
-        user["address"] = i["address"]
-        user["country"] = i["country"]
-        users.append(user)
-
-    return users
+    try:
+        for i in cur.execute('''SELECT * FROM users'''):
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
+    except:
+        create_db()
+        for i in cur.execute('''SELECT * FROM users'''):
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
+    finally:
+        return users
 
 
 def show_db_id(id):
@@ -64,38 +88,56 @@ def show_db_id(id):
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
 
-    for i in cur.execute('''SELECT * FROM users WHERE user_id == ?''', (id,)):
-        # przecinek w (id ,) bardzo ważny, inaczej błąd przy wprowadzeniu liczby dwucyfrowej
-        user = {}
-        user["user_id"] = i["user_id"]
-        user["name"] = i["name"]
-        user["email"] = i["email"]
-        user["phone"] = i["phone"]
-        user["address"] = i["address"]
-        user["country"] = i["country"]
-        users.append(user)
-
-    return users
+    try:
+        for i in cur.execute('''SELECT * FROM users WHERE user_id == ?''', (id,)):
+            # przecinek w (id ,) bardzo ważny, inaczej błąd przy wprowadzeniu liczby dwucyfrowej
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
+    except:
+        create_db()
+        for i in cur.execute('''SELECT * FROM users WHERE user_id == ?''', (id,)):
+            # przecinek w (id ,) bardzo ważny, inaczej błąd przy wprowadzeniu liczby dwucyfrowej
+            user = {}
+            user["user_id"] = i["user_id"]
+            user["name"] = i["name"]
+            user["email"] = i["email"]
+            user["phone"] = i["phone"]
+            user["address"] = i["address"]
+            user["country"] = i["country"]
+            users.append(user)
+    finally:
+        return users
 
 
 def delete(id):
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute('''DELETE FROM users WHERE user_id == ?''', (id,))
+    try:
+        cur.execute('''DELETE FROM users WHERE user_id == ?''', (id,))
 
-    conn.commit()
-    conn.close()
-
-    return f'User id:{id} Successfully deleted'
+        conn.commit()
+        conn.close()
+        return f'User id:{id} Successfully deleted'
+    except:
+        return "Error. Table or user not exist"
 
 
 def update(user, id):
     conn = connect_db()
     cur = conn.cursor()
-    cur.execute("UPDATE users SET name = ?, email = ?, phone = ?, address = ?, country = ? WHERE user_id =?",
-                (user["name"], user["email"], user["phone"], user["address"], user["country"], id))
+    try:
+        cur.execute("UPDATE users SET name = ?, email = ?, phone = ?, address = ?, country = ? WHERE user_id =?",
+                    (user["name"], user["email"], user["phone"], user["address"], user["country"], id))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
 
-    return f"User {user['name']} Successfully updated"
+        return f"User {user['name']} Successfully updated"
+    except:
+        return "Error. Table or user not exist"
